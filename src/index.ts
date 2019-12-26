7d30303fe3a42dca4e041716e0f508f5e83e6959
김병규 <doug0476@naver.com>
2019-12-27 02:20:46 +0900

    User Schema 설정

--- src/index.ts
+++ src/index.ts
@@ -6,13 +6,23 @@ createConnection()
   .then(async connection => {
     console.log(`Inserting a new user into the database...`);
 
-    const user = new User();
-    user.firstName = "Timber";
-    user.lastName = "Saw";
-    user.age = 25;
-    await connection.manager.save(user);
+    const user = await User.create({
+      email: "test13@mail.com",
+      password: "pass",
+      age: 20,
+      name: "홍길동"
+    }).save();
 
-    const users = await connection.manager.find(User);
-    console.log(`Loaded Users: ${users}`);
+    // const user = new User();
+    // user.firstName = "Timber";
+    // user.lastName = "Saw";
+    // user.age = 25;
+    // await connection.manager.save(user);
+
+    // const users = await connection.manager.find(User);
+    // console.log(`Loaded Users: ${users}`);
+    console.log(await User.find());
+
+    await connection.close();
   })
   .catch(console.error);
