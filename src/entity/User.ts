2e48a950d1cfd3a297df0bc403a1d0a1adf634ac
김병규 <doug0476@naver.com>
2019-12-29 15:30:43 +0900

    Post Entity 추가

--- src/entity/User.ts
+++ src/entity/User.ts
@@ -5,30 +5,38 @@ import {
   BaseEntity,
   CreateDateColumn,
   OneToOne,
-  JoinColumn
-} from "typeorm";
-import { Profile } from "./Profile";
+  JoinColumn,
+  OneToMany
+} from 'typeorm'
+import { Profile } from './Profile'
+import { Post } from './Post'
 
 @Entity()
 export class User extends BaseEntity {
   @PrimaryGeneratedColumn()
-  id: number;
+  id: number
 
   @Column({ unique: true })
-  email: string;
+  email: string
 
   @Column()
-  password: string;
+  password: string
 
   @CreateDateColumn()
-  createdAt: Date;
+  createdAt: Date
 
   @Column({ type: Date, nullable: true, default: null })
-  deletedAt: Date | null;
+  deletedAt: Date | null
 
   @OneToOne(
     type => Profile,
     profile => profile.user
   )
-  profile: Profile;
+  profile: Profile
+
+  @OneToMany(
+    type => Post,
+    post => post.user
+  )
+  posts: Post[]
 }
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,32 @@
-import "reflect-metadata";
-import { createConnection } from "typeorm";
-import { User } from "./entity/User";
-import { Profile } from "./entity/Profile";
+import 'reflect-metadata'
+import { createConnection } from 'typeorm'
+import { User } from './entity/User'
+import { Profile } from './entity/Profile'
+import { Post } from './entity/Post'
 
 createConnection()
   .then(async connection => {
-    console.log(`Inserting a new user into the database...`);
+    console.log(`Inserting a new user into the database...`)
+
+    const profile = await Profile.create({ name: 'kim', age: 22 }).save()
+
+    const user = await User.create({
+      email: 'tester@mail.com',
+      password: 'password',
+      profile
+    }).save()
+
+    const post = await Post.create({
+      title: '제목',
+      content: '내용',
+      user
+    }).save()
+
+    console.log(await Post.find({ relations: ['user', 'user.profile'] }))
+    await user.remove()
+
+    console.log(await Post.find())
+    console.log(await Profile.find())
 
     // const profile = await Profile.create({
     //   name: "John",
@@ -26,8 +47,8 @@ createConnection()
 
     // const users = await connection.manager.find(User);
     // console.log(`Loaded Users: ${users}`);
-    console.log(await User.find({ relations: ["profile"] }));
+    // console.log(await User.find({ relations: ['profile'] }))
 
-    await connection.close();
+    await connection.close()
   })
-  .catch(console.error);
+  .catch(console.error)
